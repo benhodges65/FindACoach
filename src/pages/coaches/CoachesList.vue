c951656e60d9ef5b617b9890c367716bbb256326
benhodges65 <77591479+benhodges65@users.noreply.github.com>
2024-02-18 22:45:31 -0600

    first commit

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        Filter
+    </section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach</router-link>
+        </div>
+        <ul>
+            List of Coaches
+        </ul>
+        List of Coaches
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
